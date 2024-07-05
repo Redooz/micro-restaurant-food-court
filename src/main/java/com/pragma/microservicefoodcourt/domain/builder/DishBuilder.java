6548a068af449c9fe1ca01da8d74b6c7e0229218
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-05 18:04:30 -0500

    [HU-3(dish)] add a builder for model dish

---
+++ src/main/java/com/pragma/microservicefoodcourt/domain/builder/DishBuilder.java
@@ -0,0 +1,52 @@
+package com.pragma.microservicefoodcourt.domain.builder;
+
+import com.pragma.microservicefoodcourt.domain.model.Dish;
+
+public class DishBuilder {
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private String urlImage;
+    private String category;
+    private String restaurantNIT;
+
+    public DishBuilder setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public DishBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public DishBuilder setPrice(Double price) {
+        this.price = price;
+        return this;
+    }
+
+    public DishBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public DishBuilder setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+        return this;
+    }
+
+    public DishBuilder setCategory(String category) {
+        this.category = category;
+        return this;
+    }
+
+    public DishBuilder setRestaurantNIT(String restaurantNIT) {
+        this.restaurantNIT = restaurantNIT;
+        return this;
+    }
+
+    public Dish createDish() {
+        return new Dish(id, name, price, description, urlImage, category, restaurantNIT);
+    }
+}
