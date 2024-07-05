29c235235461319a162ad114fc958051c0d8006a
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-05 13:26:32 -0500

    feat[HU-2(user-client)] add adapter for User API port, implements getByUserId

---
+++ src/main/java/com/pragma/microservicefoodcourt/infrastructure/driven/client/adapter/UserApiAdapter.java
@@ -0,0 +1,18 @@
+package com.pragma.microservicefoodcourt.infrastructure.driven.client.adapter;
+
+import com.pragma.microservicefoodcourt.domain.model.User;
+import com.pragma.microservicefoodcourt.domain.spi.IUserApiPort;
+import com.pragma.microservicefoodcourt.infrastructure.driven.client.api.UserFeignClient;
+import com.pragma.microservicefoodcourt.infrastructure.driven.client.mapper.IUserDtoMapper;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserApiAdapter implements IUserApiPort {
+    private final UserFeignClient userFeignClient;
+    private final IUserDtoMapper userDtoMapper;
+
+    @Override
+    public User getUserById(String id) {
+        return userDtoMapper.toUser(userFeignClient.findUserByDocumentId(id));
+    }
+}
