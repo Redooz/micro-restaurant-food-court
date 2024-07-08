7109811eeed36ea8f70b262b7c7167c8ab2ca220
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-08 11:13:01 -0500

    feat[HU-3(dish)] add dish and category handler

---
+++ src/main/java/com/pragma/microservicefoodcourt/application/handler/DishHandler.java
@@ -0,0 +1,18 @@
+package com.pragma.microservicefoodcourt.application.handler;
+
+import com.pragma.microservicefoodcourt.application.dto.request.CreateDishRequest;
+import com.pragma.microservicefoodcourt.application.mapper.IDishDtoMapper;
+import com.pragma.microservicefoodcourt.domain.api.IDishServicePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DishHandler {
+    private final IDishServicePort dishServicePort;
+    private final IDishDtoMapper dishDtoMapper;
+
+    public void createDish(CreateDishRequest request) {
+        dishServicePort.saveDish(dishDtoMapper.toModel(request));
+    }
+}
