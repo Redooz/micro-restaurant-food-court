d9df400ba88d67a545057f7993750dfee545e28f
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-08 09:41:39 -0500

    feat[HU-3(dish)] add entities for dish and category

---
+++ src/main/java/com/pragma/microservicefoodcourt/infrastructure/driven/jpa/mysql/entity/DishEntity.java
@@ -0,0 +1,36 @@
+package com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dishes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DishEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    private String description;
+
+    private String urlImage;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private CategoryEntity category;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_nit")
+    private RestaurantEntity restaurant;
+}
