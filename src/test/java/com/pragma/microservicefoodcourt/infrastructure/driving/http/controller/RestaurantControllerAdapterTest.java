709d4a5a5010a01b33ce371f5944bc602b4b4ac3
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-09 15:00:40 -0500

    test[HU-9(restaurant)] add tests in controller and use case for finding all restaurants

--- src/test/java/com/pragma/microservicefoodcourt/infrastructure/driving/http/controller/RestaurantControllerAdapterTest.java
+++ src/test/java/com/pragma/microservicefoodcourt/infrastructure/driving/http/controller/RestaurantControllerAdapterTest.java
@@ -1,6 +1,7 @@
 package com.pragma.microservicefoodcourt.infrastructure.driving.http.controller;
 
 import com.pragma.microservicefoodcourt.application.dto.request.CreateRestaurantRequest;
+import com.pragma.microservicefoodcourt.application.dto.response.GetRestaurantResponse;
 import com.pragma.microservicefoodcourt.application.handler.RestaurantHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -39,5 +42,14 @@ class RestaurantControllerAdapterTest {
 
         verify(restaurantHandler, times(1)).createRestaurant(request);
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Should find all restaurants and return status 200")
+    void shouldFindAllRestaurantsAndReturnStatus200() {
+        ResponseEntity<List<GetRestaurantResponse>> response = restaurantControllerAdapter.findAllRestaurants(1, 10);
+
+        verify(restaurantHandler, times(1)).findAllRestaurants(1, 10);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
