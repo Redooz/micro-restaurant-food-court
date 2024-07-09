9f6bdc5ca165b255fba01f39e541a4092038af71
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-09 14:22:10 -0500

    feat[HU-9(restaurant)] add to persistence adapter and repository methods for finding all restaurants, also add a method to mapper for mapping from entities list to model list

--- src/main/java/com/pragma/microservicefoodcourt/infrastructure/driven/jpa/mysql/adapter/RestaurantPersistenceAdapter.java
+++ src/main/java/com/pragma/microservicefoodcourt/infrastructure/driven/jpa/mysql/adapter/RestaurantPersistenceAdapter.java
@@ -2,11 +2,16 @@ package com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.adapter
 
 import com.pragma.microservicefoodcourt.domain.model.Restaurant;
 import com.pragma.microservicefoodcourt.domain.spi.IRestaurantPersistencePort;
+import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.entity.RestaurantEntity;
 import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.mapper.IRestaurantEntityMapper;
 import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.repository.IRestaurantRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -27,5 +32,14 @@ public class RestaurantPersistenceAdapter implements IRestaurantPersistencePort 
     @Override
     public Optional<Restaurant> findByNit(String nit) {
         return restaurantRepository.findById(nit).map(restaurantEntityMapper::toModel);
+    }
+
+    @Override
+    public List<Restaurant> findAll(int page, int size) {
+        Sort sort = Sort.by(Sort.Direction.ASC, "name");
+        Pageable pageable = PageRequest.of(page, size, sort);
+        List<RestaurantEntity> restaurantEntities = restaurantRepository.findAll(pageable).getContent();
+
+        return restaurantEntityMapper.toModelList(restaurantEntities);
     }
 }
