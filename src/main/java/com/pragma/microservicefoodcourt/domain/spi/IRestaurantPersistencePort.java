a396456eebf77c4ce3b12e9aa7207386f88fef47
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-09 14:10:56 -0500

    feat[HU-9(restaurant)] add to persistence port findAll, missing implementation in adapter

--- src/main/java/com/pragma/microservicefoodcourt/domain/spi/IRestaurantPersistencePort.java
+++ src/main/java/com/pragma/microservicefoodcourt/domain/spi/IRestaurantPersistencePort.java
@@ -2,10 +2,12 @@ package com.pragma.microservicefoodcourt.domain.spi;
 
 import com.pragma.microservicefoodcourt.domain.model.Restaurant;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IRestaurantPersistencePort {
     void saveRestaurant(Restaurant restaurant);
     Optional<Restaurant> findByName(String name);
     Optional<Restaurant> findByNit(String nit);
+    List<Restaurant> findAll(int page, int size);
 }
