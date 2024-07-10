327c779e2e78455c70c0439bf1685c4a5f24ca9c
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-10 11:18:37 -0500

    feat[HU-11(order)] add method to create orders in order handler

---
+++ src/main/java/com/pragma/microservicefoodcourt/application/handler/OrderHandler.java
@@ -0,0 +1,25 @@
+package com.pragma.microservicefoodcourt.application.handler;
+
+import com.pragma.microservicefoodcourt.application.dto.request.CreateOrderRequest;
+import com.pragma.microservicefoodcourt.application.mapper.IOrderDtoMapper;
+import com.pragma.microservicefoodcourt.domain.api.IOrderServicePort;
+import com.pragma.microservicefoodcourt.domain.model.Order;
+import com.pragma.microservicefoodcourt.domain.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderHandler {
+    private final IOrderServicePort orderServicePort;
+    private final IOrderDtoMapper orderDtoMapper;
+
+    public void createOrder(CreateOrderRequest request) {
+        User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Order order = orderDtoMapper.toModelFromCreate(request);
+
+        order.setClient(loggedUser);
+        orderServicePort.saveOrder(order);
+    }
+}
