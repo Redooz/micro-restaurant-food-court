ab1661dda3bad4e90facf13c6655e52dcf7fea28
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-10 16:08:44 -0500

    feat[HU-11(order)] add to dto validations

--- src/main/java/com/pragma/microservicefoodcourt/application/dto/OrderDishItemDto.java
+++ src/main/java/com/pragma/microservicefoodcourt/application/dto/OrderDishItemDto.java
@@ -1,7 +1,13 @@
 package com.pragma.microservicefoodcourt.application.dto;
 
+import com.pragma.microservicefoodcourt.application.constant.OrderReqConstant;
+
+import javax.validation.constraints.Positive;
+
 public record OrderDishItemDto(
-    DishItemDto dish,
-    int quantity
+        DishItemDto dish,
+
+        @Positive(message = OrderReqConstant.MSG_QUANTITY_MUST_BE_POSITIVE)
+        int quantity
 ) {
 }
--- src/main/java/com/pragma/microservicefoodcourt/application/dto/request/CreateOrderRequest.java
+++ src/main/java/com/pragma/microservicefoodcourt/application/dto/request/CreateOrderRequest.java
@@ -1,12 +1,16 @@
 package com.pragma.microservicefoodcourt.application.dto.request;
 
+import com.pragma.microservicefoodcourt.application.constant.OrderReqConstant;
 import com.pragma.microservicefoodcourt.application.dto.OrderDishItemDto;
 import com.pragma.microservicefoodcourt.application.dto.RestaurantItemDto;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record CreateOrderRequest(
-    List<OrderDishItemDto> orderDishes,
-    RestaurantItemDto restaurant
+        @NotEmpty(message = OrderReqConstant.MSG_ORDER_DISHES_NOT_EMPTY)
+        List<OrderDishItemDto> orderDishes,
+
+        RestaurantItemDto restaurant
 ) {
 }
