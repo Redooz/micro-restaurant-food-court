1e4f329310d522e5487b6e51506690cd82d3d950
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-11 08:40:58 -0500

    feat[HU-12(order)] add to persistance adapter and repository a method to find all orders by status and restaurant

--- src/main/java/com/pragma/microservicefoodcourt/domain/spi/IOrderPersistencePort.java
+++ src/main/java/com/pragma/microservicefoodcourt/domain/spi/IOrderPersistencePort.java
@@ -1,6 +1,8 @@
 package com.pragma.microservicefoodcourt.domain.spi;
 
 import com.pragma.microservicefoodcourt.domain.model.Order;
+import com.pragma.microservicefoodcourt.domain.model.OrderStatus;
+import com.pragma.microservicefoodcourt.domain.model.Restaurant;
 import com.pragma.microservicefoodcourt.domain.model.User;
 
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 public interface IOrderPersistencePort {
     void saveOrder(Order order);
     List<Order> findAllOrdersByClientId(User user);
+    List<Order> findAllOrdersByStatusAndRestaurant(OrderStatus status, Restaurant restaurant, int page, int size);
 }
--- src/main/java/com/pragma/microservicefoodcourt/infrastructure/driven/jpa/mysql/adapter/OrderPersistenceAdapter.java
+++ src/main/java/com/pragma/microservicefoodcourt/infrastructure/driven/jpa/mysql/adapter/OrderPersistenceAdapter.java
@@ -1,14 +1,21 @@
 package com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.adapter;
 
 import com.pragma.microservicefoodcourt.domain.model.Order;
+import com.pragma.microservicefoodcourt.domain.model.OrderStatus;
+import com.pragma.microservicefoodcourt.domain.model.Restaurant;
 import com.pragma.microservicefoodcourt.domain.model.User;
 import com.pragma.microservicefoodcourt.domain.spi.IOrderPersistencePort;
 import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.entity.OrderDishEntity;
 import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.entity.OrderEntity;
+import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.entity.RestaurantEntity;
 import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.mapper.IOrderEntityMapper;
+import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.mapper.IRestaurantEntityMapper;
 import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.repository.IOrderDishRepository;
 import com.pragma.microservicefoodcourt.infrastructure.driven.jpa.mysql.repository.IOrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -17,6 +24,7 @@ public class OrderPersistenceAdapter implements IOrderPersistencePort {
     private final IOrderRepository orderRepository;
     private final IOrderDishRepository orderDishRepository;
     private final IOrderEntityMapper orderEntityMapper;
+    private final IRestaurantEntityMapper restaurantEntityMapper;
 
     @Override
     public void saveOrder(Order order) {
@@ -34,6 +42,17 @@ public class OrderPersistenceAdapter implements IOrderPersistencePort {
     @Override
     public List<Order> findAllOrdersByClientId(User user) {
         List<OrderEntity> orderEntities = orderRepository.findAllByClientId(user.getDocumentId());
+
+        return orderEntityMapper.toModelList(orderEntities);
+    }
+
+    @Override
+    public List<Order> findAllOrdersByStatusAndRestaurant(OrderStatus status, Restaurant restaurant, int page, int size) {
+        RestaurantEntity restaurantEntity = restaurantEntityMapper.toEntity(restaurant);
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        List<OrderEntity> orderEntities = orderRepository.findAllByStatusAndRestaurant(status, restaurantEntity, pageable).getContent();
 
         return orderEntityMapper.toModelList(orderEntities);
     }
