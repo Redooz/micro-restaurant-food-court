132b0c90559c4b20d4836d02b4b332e906e086af
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-15 13:52:26 -0500

    feat[HU-17(traceability)] add dtos for traceability api

---
+++ src/main/java/com/pragma/microservicefoodcourt/infrastructure/driven/client/dto/response/GetTraceabilityResponse.java
@@ -0,0 +1,18 @@
+package com.pragma.microservicefoodcourt.infrastructure.driven.client.dto.response;
+
+import com.pragma.microservicefoodcourt.domain.model.enums.OrderStatus;
+
+import java.time.LocalDate;
+
+public record GetTraceabilityResponse(
+        Long id,
+        Long orderId,
+        String clientId,
+        String clientEmail,
+        String employeeId,
+        String employeeEmail,
+        LocalDate date,
+        OrderStatus lastStatus,
+        OrderStatus newStatus
+) {
+}
